8f6bc9f1674ae1fad028d0e80d74fbd02a955495
andre4130 <60257960+andre4130@users.noreply.github.com>
2020-03-06 09:58:43 +0100

    ejercicio letras repetidas

--- letrasRepetidas.js
+++ letrasRepetidas.js
@@ -1,36 +1,37 @@
 // ejercicio Letras Repetidas
 
-// Fase 1
-// var nombre = "André de Mendonça Maia";
-// console.log(nombre);
-// for (var i = 0; i < nombre.length; i++) {
-//   console.log(nombre[i]);
-// }
-//
-// //Fase 2
-// var nombre = "André de Mendonça Maia";
-// var listaVocales = 'aeiouAEIOU';
-// var listaNumeros = '0123456789'
-// console.log("Partimos del nombre: " + nombre);
-// for (var i = 0; i < nombre.length; i++) {
-//   if (listaVocales.indexOf(nombre[i])!== -1) {
-//     console.log("He encontrado la vocal "+ nombre[i])
-//     ;
-//   }
-//   else if (listaNumeros.indexOf(nombre[i])!== -1){
-//     console.log(nombre[i]+" no es una letra del nombre")
-//   }
-//   else if (nombre[i] == " ") {
-//     console.log("Aquí hay un espacio")
-//   }
-//   else {
-//     console.log("He encontrado la consonante "+ nombre[i]);
-//   }
-// }
+//Fase 1
+var nombre = "André de Mendonça Maia";
+console.log(nombre);
+for (var i = 0; i < nombre.length; i++) {
+  console.log(nombre[i]);
+}
+
+//Fase 2
+
+var nombre = "André de Mendonça Maia";
+var listaVocales = 'aeiouAEIOU';
+var listaNumeros = '0123456789'
+console.log("Partimos del nombre: " + nombre);
+for (var i = 0; i < nombre.length; i++) {
+  if (listaVocales.indexOf(nombre[i])!== -1) {
+    console.log("He encontrado la vocal "+ nombre[i])
+    ;
+  }
+  else if (listaNumeros.indexOf(nombre[i])!== -1){
+    console.log(nombre[i]+" no es una letra del nombre")
+  }
+  else if (nombre[i] == " ") {
+    console.log("Aquí hay un espacio")
+  }
+  else {
+    console.log("He encontrado la consonante "+ nombre[i]);
+  }
+}
 
 //Fase 3
 
-let nombre = "Anddrened"
+let nombre = "André de Mendonça Maia"
 nombre = nombre.split('');
 let nombreMap = new Map();
 
@@ -46,35 +47,14 @@ for (var i = 0; i < nombre.length; i++) {
 console.log(nombreMap);
 
 
-//
+// Fase 4
 
-// var repetidas = [];
-// var contadorLetras =0;
-// var count = -1;
-// for (var i = 0; i < tempArray.length; i++) {
-//   for (var j = -1; j < repetidas.length; j++){
-//     if (tempArray[i] !== repetidas[j]){
-//       count ++;
-//       repetidas.push(tempArray[i]);
-//         console.log(repetidas);
-//         console.log(count);
-//     }
-//   }
-// }
+let nombre1 = "André";
+let apellido = "Maia";
+nombre1 = nombre1.split('');
+apellido = apellido.split('');
+espacio = " ";
 
+fullName = nombre1.concat(espacio, apellido);
 
-// codigo antiguo
-
-// for (var i = 0; i < nombre.length; i++) {
-//   if (nombre[i] !== array[contador]) {
-//     console.log("Nueva letra: "+ nombre[i]);
-//     const array = nombre.map(i => nombre[i]);
-//     contador +=1;
-//     console.log(contador);
-//     console.log(array);
-//     }
-//   else {
-//     console.log("la letra "+ nombre[i]+" ya está almacenada.");
-//   }
-// }
-// console.log(map);
+console.log(fullName);
